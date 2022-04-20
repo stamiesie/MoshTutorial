d2747c0285b32e6f71e8b411e8bd78d29c95ad4b
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-04-20 15:58:45 -0700

    add calculateBalance method for remaining payments

--- src/com/moshtutorial/MoshTutorial.java
+++ src/com/moshtutorial/MoshTutorial.java
@@ -1,18 +1,34 @@
 package com.moshtutorial;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MoshTutorial {
+    final static byte MONTHS = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
         int principal = (int) readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
         float apr = (float) readNumber("Annual Interest Rate: ", 0, 30);
         byte years = (byte) readNumber("Period (Years): ", 0, 30);
 
         double mortgage = calculateMortgage(principal, apr, years);
-
         String payment = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + payment);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + payment);
+
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        for (short month = 1; month <= years * MONTHS; month++) {
+            double balance = calculateBalance(principal, apr, years, month);
+                String remainder = NumberFormat.getCurrencyInstance().format(balance);
+                System.out.println(remainder);
+        }
     }
 
     public static double readNumber(String prompt, int min, int max) {
@@ -32,9 +48,6 @@ public class MoshTutorial {
             float apr,
             byte years) {
 
-        final byte MONTHS = 12;
-        final byte PERCENT = 100;
-
         float monthlyInterest = apr / PERCENT / MONTHS;
         short totalPayments = (short)(years * MONTHS);
 
@@ -43,5 +56,21 @@ public class MoshTutorial {
                 / (Math.pow(1 + monthlyInterest, totalPayments) - 1));
 
         return mortgage;
+    }
+
+    public static double calculateBalance(
+            int principal,
+            float apr,
+            byte years,
+            short numberOfPaymentsMade) {
+
+        float monthlyInterest = apr / PERCENT / MONTHS;
+        short totalPayments = (short)(years * MONTHS);
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, totalPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, totalPayments) - 1);
+
+        return balance;
     }
 }
