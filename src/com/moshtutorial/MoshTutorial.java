602bb63f86c2e169de603fef8f1f1d1c706ba121
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-04-20 13:31:26 -0700

    add basic error handling

--- src/com/moshtutorial/MoshTutorial.java
+++ src/com/moshtutorial/MoshTutorial.java
@@ -1,27 +1,48 @@
 package com.moshtutorial;
 
 
-import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MoshTutorial {
     public static void main(String[] args) {
+
         final byte MONTHS = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int totalPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Number out of range");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float apr  = scanner.nextFloat();
-        float monthlyInterest = apr / PERCENT / MONTHS;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float apr  = scanner.nextFloat();
+            if (apr > 0 && apr <= 30) {
+                monthlyInterest = apr / PERCENT / MONTHS;
+                break;
+            }
+            System.out.println("Number out of range");
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int totalPayments = years * MONTHS;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years > 0 && years <= 30) {
+                totalPayments = years * MONTHS;
+                break;
+            }
+            System.out.println("Number out of range");
+        }
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, totalPayments)
