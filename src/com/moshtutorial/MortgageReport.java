72f264a079b9e04f3d74d31c603569868ab351fd
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-04-21 14:05:55 -0700

    refactor duplicate currencyInstance into field in MortgageReport class

--- src/com/moshtutorial/MortgageReport.java
+++ src/com/moshtutorial/MortgageReport.java
@@ -4,24 +4,24 @@ package com.moshtutorial;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
-
     public void printPaymentSchedule() {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for(double balance : calculator.getRemainingBalances())
-        System.out.println("Balance: " + NumberFormat.getCurrencyInstance().format(balance));
+        System.out.println("Balance: " + currency.format(balance));
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String payment = NumberFormat.getCurrencyInstance().format(mortgage);
+        String payment = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
