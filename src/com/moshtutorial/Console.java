41074a259b46a96cc08b5d2683ada46fcc8f7808
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-04-21 12:48:28 -0700

    create overload for readNumber method in Console class

--- src/com/moshtutorial/Console.java
+++ src/com/moshtutorial/Console.java
@@ -3,12 +3,15 @@ package com.moshtutorial;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
     public static double readNumber(String prompt, int min, int max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextFloat();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " and " + max);
